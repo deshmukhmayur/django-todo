081916a53d392b7288a390a7524eb243297aacff
deshmukhmayur <deshmukhmayur@outlook.com>
2017-01-18 01:23:14 +0530

    URL namespace to fix the problem with the api calls from any base app URL
    Added a rootURL variable that will store the root address of the app, and
    will be used in app.js to make the api calls.
    This makes the api calls from app.js work for any URL the app is on in the
    project urls.py.

--- todo/static/todo/javascripts/app.js
+++ todo/static/todo/javascripts/app.js
@@ -1,10 +1,16 @@
+if (!rootURL) {
+    rootURL = '/';
+    // if the rootURL is not initialized in the template,
+    // it defaults to '/'
+}
+
 angular.module('nodeTodo', [])
 .controller('mainController', ($scope, $http) => {
     $scope.formData = {};
     $scope.todoData = {};
 
     // Get all todos
-    $http.get('/api/v1/todo/get/')
+    $http.get(rootURL + 'api/v1/get/')
     .then((data) => {
         $scope.todoData = data;
         console.log(data);
@@ -16,7 +22,7 @@ angular.module('nodeTodo', [])
     // Create a new todo
     $scope.createTodo = () => {
         // console.log($scope.formData);
-        $http.post('/api/v1/todo/add/', $scope.formData)
+        $http.post(rootURL + 'api/v1/add/', $scope.formData)
         .then((data) => {
             $scope.formData = {};
             $scope.todoData = data;
@@ -30,7 +36,7 @@ angular.module('nodeTodo', [])
     // Update todo
     $scope.updateTodo = (todo) => {
         // console.log(!todo.complete);
-        $http.put('/api/v1/todo/update/' + todo.id + '/', {"text": todo.text, "complete": !todo.complete})
+        $http.put(rootURL + 'api/v1/update/' + todo.id + '/', {"text": todo.text, "complete": !todo.complete})
         .then((data) => {
             $scope.todoData = data;
             console.log(data);
@@ -42,7 +48,7 @@ angular.module('nodeTodo', [])
 
     // Delete a todo
     $scope.deleteTodo = (todoID) => {
-        $http.delete('/api/v1/todo/delete/' + todoID + '/')
+        $http.delete(rootURL + 'api/v1/delete/' + todoID + '/')
         .then((data) => {
             $scope.todoData = data;
             console.log(data);
--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url, include
 from . import views, apis
 
+app_name='todo'
+
 urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^api/v1/todo/get/', apis.read, name='read'),
-    url(r'^api/v1/todo/add/$', apis.create, name='create'),
-    url(r'^api/v1/todo/update/(?P<todoID>\d+)/$', apis.update, name='update'),
-    url(r'^api/v1/todo/delete/(?P<todoID>\d+)/$', apis.delete, name='delete'),
+    url(r'^$', views.index, name='index'),
+    url(r'^api/v1/get/', apis.read, name='read'),
+    url(r'^api/v1/add/$', apis.create, name='create'),
+    url(r'^api/v1/update/(?P<todoID>\d+)/$', apis.update, name='update'),
+    url(r'^api/v1/delete/(?P<todoID>\d+)/$', apis.delete, name='delete'),
 ]
